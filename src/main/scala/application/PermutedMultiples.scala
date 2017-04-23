c87aaac043111bb19a1a720ef73ef910ae9deeb9
Kaique da Silva <kaique.developer@gmail.com>
2017-04-23 08:25:26 -0300

    Supported the setter for x variable

--- src/main/scala/application/PermutedMultiples.scala
+++ src/main/scala/application/PermutedMultiples.scala
@@ -1,5 +1,13 @@
 package application
 
-class PermutedMultiples extends Object with PermutedMultiplesDefinition {}
+class PermutedMultiples extends Object with PermutedMultiplesDefinition {
+  
+}
 
-trait PermutedMultiplesDefinition {}
+trait PermutedMultiplesDefinition {
+  var x: Int = 10
+  def setX(v: Int): Int = {
+    x = v
+    v
+  }
+}
--- src/test/scala/application/PermutedMultiplesSpec.scala
+++ src/test/scala/application/PermutedMultiplesSpec.scala
@@ -8,4 +8,12 @@ class PermutedMultiplesSpec extends FlatSpec with Matchers {
   "The permuted multiples class" should "have an instance type" in {
     assume(permutedMultiples.isInstanceOf[PermutedMultiples])
   }
+
+  "The permuted multiples class" should "have an x variables" in {
+    permutedMultiples.x shouldEqual 10
+  }
+
+  "The permuted multiples class" should "have an setter for x" in {
+    permutedMultiples.setX(15) shouldEqual 15
+  }
 }
